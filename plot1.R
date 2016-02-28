9e140f1901382f82db935300493c48f28b57f1e9
Thomas Chen <thomasdechen@gmail.com>
2016-02-28 11:00:19 +0800

    EDA Course Project 1

--- plot1.R
+++ plot1.R
@@ -1,4 +1,4 @@
-dataFile <- "/Documents/DataSci/EDA/household_power_consumption.txt"
+dataFile <- "./EDA/household_power_consumption.txt"
 data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
 subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 globalActivePower <- as.numeric(subSetData$Global_active_power)
